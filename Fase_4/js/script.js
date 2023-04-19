b2fe1427f057bf04a06d931b59fef75e8cc33079
Edysonn <125694532+Edysonn@users.noreply.github.com>
2023-04-18 22:26:42 -0500

    act 1 18 de abril de 2023
    Añade fase 4 cuestionarios.

---
+++ Fase_4/js/script.js
@@ -0,0 +1,32 @@
+function procesarRespuestas() {
+    let total = 5
+    let puntos= 0;
+    
+
+    let myForm = document.forms["formulario"];
+    let RespuestasCorrectas = ["a","c","b","a","b"];
+
+    for (let i = 1; i <= total; i++) {
+        if (myForm["r"+i].value == null || 
+            myForm["r"+i].value == '') {
+            alert('Tal vez olvidaste responder una o varias preguntas');
+            return false;
+        } else {
+            if (myForm["r" + i].value === RespuestasCorrectas[i - 1])
+            puntos ++;
+        }
+    }
+    let resultado1 = document.getElementById('resultado1')
+    resultado1.innerHTML='Obtuviste '+ puntos +' puntos de '+ total +' posibles';
+    return false;
+
+}
+
+const limpiarSeleccion = (name) => {//cada vez que se invoque se limpiará el radio button unicamente con el mismo nombre.
+    const radioBtns = document.querySelectorAll("input[type='radio'][name='"+ name + "']");
+    radioBtns.forEach((radioBtn) =>{
+        if(radioBtn.checked === true) radioBtn.checked = false;//si hay selección, este se reiniciará
+        resultado1.innerHTML='aquí se verá su resultado después de dar clic al botón "enviar"';// pantalla de mensaje
+        return false;
+    })
+}
