a02cb8b22454128111b371497f8fc1f160ebe019
Edysonn <125694532+Edysonn@users.noreply.github.com>
2023-05-11 23:25:36 -0500

    Act 1 11 de mayo de 2023
    Compilacion uno faltante

--- Fase_4/js/script.js
+++ Fase_4/js/script.js
@@ -21,6 +21,52 @@ function procesarRespuestas() {
     return false;
 
 }
+function procesarRespuestas2() {
+    let total = 5
+    let puntos= 0;
+    
+
+    let myForm = document.forms["formulario"];
+    let RespuestasCorrectas = ["b","a","b","b","a"];
+
+    for (let i = 1; i <= total; i++) {
+        if (myForm["r"+i].value == null || 
+            myForm["r"+i].value == '') {
+            alert('Debe responder todas la preguntas antes de poder enviar sus respuestas');
+            return false;
+        } else {
+            if (myForm["r" + i].value === RespuestasCorrectas[i - 1])
+            puntos ++;
+        }
+    }
+    let resultado1 = document.getElementById('resultado1')
+    resultado1.innerHTML='Obtuviste '+ puntos +' puntos de '+ total +' posibles';
+    return false;
+
+}
+function procesarRespuestas3() {
+    let total = 5
+    let puntos= 0;
+    
+
+    let myForm = document.forms["formulario"];
+    let RespuestasCorrectas = ["a","c","b","c","a"];
+
+    for (let i = 1; i <= total; i++) {
+        if (myForm["r"+i].value == null || 
+            myForm["r"+i].value == '') {
+            alert('Favor responder todas las preguntas');
+            return false;
+        } else {
+            if (myForm["r" + i].value === RespuestasCorrectas[i - 1])
+            puntos ++;
+        }
+    }
+    let resultado1 = document.getElementById('resultado1')
+    resultado1.innerHTML='Obtuviste '+ puntos +' puntos de '+ total +' posibles';
+    return false;
+
+}
 
 const limpiarSeleccion = (name) => {//cada vez que se invoque se limpiará el radio button unicamente con el mismo nombre.
     const radioBtns = document.querySelectorAll("input[type='radio'][name='"+ name + "']");
