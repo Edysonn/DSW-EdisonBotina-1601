7d40821954d20229bdd8c375dec4f594ed7a2dc5
Edysonn <125694532+Edysonn@users.noreply.github.com>
2023-05-13 01:50:49 -0500

    act 1 13 de mayo de 2023
    ultimo diseño.

--- Fase_4/js/script.js
+++ Fase_4/js/script.js
@@ -67,6 +67,29 @@ function procesarRespuestas3() {
     return false;
 
 }
+function procesarRespuestas4() {
+    let total = 5
+    let puntos= 0;
+    
+
+    let myForm = document.forms["formulario"];
+    let RespuestasCorrectas = ["c","a","b","b","c"];
+
+    for (let i = 1; i <= total; i++) {
+        if (myForm["r"+i].value == null || 
+            myForm["r"+i].value == '') {
+            alert('Favor responder todas las preguntas');
+            return false;
+        } else {
+            if (myForm["r" + i].value === RespuestasCorrectas[i - 1])
+            puntos ++;
+        }
+    }
+    let resultado1 = document.getElementById('resultado1')
+    resultado1.innerHTML='Obtuviste '+ puntos +' puntos de '+ total +' posibles';
+    return false;
+
+}
 
 const limpiarSeleccion = (name) => {//cada vez que se invoque se limpiará el radio button unicamente con el mismo nombre.
     const radioBtns = document.querySelectorAll("input[type='radio'][name='"+ name + "']");
